1f43c4ac21e0d89068bb62b19162c5d811605f17
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-05-11 12:19:17 -0300

    updated tecnologies styles

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -47,99 +47,113 @@ function Technologies() {
 					variants={iconVariants(2.5)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<RiReactjsLine className='text-7xl text-cyan-400' />
+					<span className='text-center text-sm  text-cyan-400 '>React JS</span>
 				</motion.div>
 				<motion.div
 					variants={iconVariants(3)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<FaNode className='text-7xl text-green-600' />
+					<span className='text-center text-sm text-green-600 '>Node JS</span>
 				</motion.div>
 				<motion.div
 					variants={iconVariants(5)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<SiExpress className='text-7xl' />
+					<span className='text-center text-sm  '>Express JS</span>
 				</motion.div>
 				<motion.div
 					variants={iconVariants(2)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<SiMongodb className='text-7xl text-green-500' />
+					<span className='text-center text-sm  text-green-500'>MongoDB</span>
 				</motion.div>
 				<motion.div
 					variants={iconVariants(6)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<SiTypescript className='text-7xl text-blue-600' />
+					<span className='text-center text-sm  text-blue-600'>Typescript</span>
 				</motion.div>
 				<motion.div
 					variants={iconVariants(4)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<FaAngular className='text-7xl text-rose-700' />
+					<span className='text-center text-sm  text-rose-700'>Angular</span>
 				</motion.div>
 				<motion.div
 					variants={iconVariants(3)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<FaJava className='text-7xl text-red-600' />
+					<span className='text-center text-sm  text-red-600'>Java</span>
 				</motion.div>
 				<motion.div
 					variants={iconVariants(2.5)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<SiSpring className='text-7xl text-lime-600' />
+					<span className='text-center text-sm text-lime-600'>Spring Boot</span>
 				</motion.div>
 				<motion.div
 					variants={iconVariants(5)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<SiMysql className='text-7xl text-amber-500' />
+					<span className='text-center text-sm  text-amber-500'>Mysql</span>
 				</motion.div>
 				<motion.div
 					variants={iconVariants(4)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<SiJest className='text-7xl text-pink-700' />
+					<span className='text-center text-sm text-pink-700'>Jest</span>
 				</motion.div>
 				<motion.div
 					variants={iconVariants(2)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<FaGit className='text-7xl' />
+					<span className='text-center text-sm'>Git</span>
 				</motion.div>
 				<motion.div
 					variants={iconVariants(6)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<SiTailwindcss className='text-7xl text-cyan-400' />
+					<span className='text-center text-sm text-cyan-400'>Tailwind</span>
 				</motion.div>
 				<motion.div
 					variants={iconVariants(2)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<SiMui className='text-7xl text-blue-600' />
+					<span className='text-center text-sm text-blue-600'>Material UI</span>
 				</motion.div>
 				<motion.div
 					variants={iconVariants(4)}
 					initial='initial'
 					animate='animate'
-					className='rounded-2xl border-4 border-neutral-800 p-4'>
+					className='rounded-2xl border-4 border-neutral-800 p-4 flex flex-col justify-center'>
 					<RiBootstrapFill className='text-7xl text-violet-700' />
+					<span className='text-center text-sm text-violet-700'>Bootstrap</span>
 				</motion.div>
 			</motion.div>
 		</div>
